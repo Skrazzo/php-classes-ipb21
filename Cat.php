66cbc91ce4ca42cc31de08ed75b714a76fe0e80f
Leons Aleksandrovs <skrazzo@proton.me>
2023-11-10 12:56:58 +0200

    Update Cat.php

--- Cat.php
+++ Cat.php
@@ -1,14 +1,1 @@
-<?php
-
-class Cat{
-    public $name;
-    private $age = 0;
-
-    function birthday(){
-        $this->age++;
-    }
-
-    static function moew(){
-        print('"Mewo~" 🐾💕 ');
-    }
-}
+<?php class Cat{ public $name; private $age = 0; function birthday(){ $this->age++; } static function moew(){ print('"Mewo~" 🐾💕 '); } }
