67f251aaaa7c24f833964eed2b0c8de66ab3b30f
Leons Aleksandrovs <skrazzo@proton.me>
2023-11-10 12:56:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-system('clear');
-
-include 'Cat.php';
-
-$cat1 = new Cat; $cat1->name = 'Mincis';
-$cat2 = new Cat; $cat2->name = 'Brincis';
-
-
-Cat::moew();
+system('clear'); include 'Cat.php'; $cat1 = new Cat; $cat1->name = 'Mincis'; $cat2 = new Cat; $cat2->name = 'Brincis'; Cat::moew();
 
 
